7859f913d92f9f9562d933722ecabfc3beaaa7ac
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-08-15 15:09:59 -0400

    add boilderplate read code

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
 use std::{fs, path::Path, sync::Arc};
 
 use parquet::{
+    data_type::Int32Type,
     file::{properties::WriterProperties, writer::SerializedFileWriter},
-    schema::parser::parse_message_type, data_type::Int32Type,
+    schema::parser::parse_message_type,
+    file::reader::{FileReader, SerializedFileReader},
 };
 
-fn main() {
+fn write_file() {
     let path = Path::new("./sample.parquet");
 
     let message_type = "
   message schema {
-    REQUIRED INT32 b;
+    REQUIRED INT32 nums;
   }
 ";
     let schema = Arc::new(parse_message_type(message_type).unwrap());
 
-
     let file = fs::File::create(&path).unwrap();
 
     let mut writer = SerializedFileWriter::new(file, schema, Default::default()).unwrap();
-
 
     let mut row_group_writer = writer.next_row_group().unwrap();
     while let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
         // ... write values to a column writer
         let values: [i32; 5] = [1, 2, 3, 4, 5];
 
-        col_writer.typed::<Int32Type>().write_batch(&values, None, None).unwrap();
+        col_writer
+            .typed::<Int32Type>()
+            .write_batch(&values, None, None)
+            .unwrap();
         col_writer.close().unwrap()
     }
     row_group_writer.close().unwrap();
@@ -34,4 +37,27 @@ fn main() {
 
     let bytes = fs::read(&path).unwrap();
     assert_eq!(&bytes[0..4], &[b'P', b'A', b'R', b'1']);
+}
+
+fn read_file() {
+    let path = Path::new("./sample.parquet");
+
+    if let Ok(file) = fs::File::open(&path) {
+        let reader = SerializedFileReader::new(file).unwrap();
+
+        let parquet_metadata = reader.metadata();
+        // println!("parquet_metadata: {:?}", parquet_metadata);
+
+        assert_eq!(parquet_metadata.num_row_groups(), 1);
+
+        let row_group_reader = reader.get_row_group(0).unwrap();
+        println!("num columns: {}", row_group_reader.num_columns());
+
+
+    }
+}
+
+fn main() {
+    write_file();
+    read_file();
 }
